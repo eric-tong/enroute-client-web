93b31b0031e53f04b16488fbcdb5bfa2ea1e7543
eric-tong <erictong.designs@gmail.com>
2019-11-13 20:42:18 +0000

    ENR-0 Refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App.react";
+import "./styles/index.scss";
+import App from "./components/App.react";
 
 ReactDOM.render(<App />, document.getElementById("root"));
