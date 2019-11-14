885a450f66c504ea34da97b4e72685e0c9ae203a
eric-tong <erictong.designs@gmail.com>
2019-11-14 10:16:33 +0000

    ENR-14 Add viewport

--- src/components/map/Map.react.js
+++ src/components/map/Map.react.js
@@ -5,6 +5,7 @@ import ReactMapGL from "react-map-gl";
 import VehicleMarker from "./VehicleMarker.react";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import initialViewport from "../../styles/viewport";
 
 const VEHICLE = gql`
   {
@@ -18,13 +19,7 @@ const VEHICLE = gql`
 `;
 
 function Map() {
-  const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
-    zoom: 12,
-    latitude: 51.7559253,
-    longitude: -1.2526899,
-  });
+  const [viewport, setViewport] = useState(initialViewport);
   const { data, loading, error } = useQuery(VEHICLE, { pollInterval: 10000 });
   const coords = !loading && !error && data ? data.vehicle.coords : null;
 
---
+++ src/styles/viewport.js
@@ -0,0 +1,11 @@
+// @flow
+
+const viewport = {
+  width: "100vw",
+  height: "100vh",
+  zoom: 12,
+  latitude: 51.78025411880868,
+  longitude: -1.274807466138682,
+};
+
+export default viewport;
