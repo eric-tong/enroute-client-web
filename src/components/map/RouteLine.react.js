0afad513d08a1b6d26a7b06942aeda07aafca4c5
eric-tong <erictong.designs@gmail.com>
2019-11-15 17:43:58 +0000

    ENR-16 Get route from server

--- src/components/map/RouteLine.react.js
+++ src/components/map/RouteLine.react.js
@@ -1,33 +1,51 @@
 // @flow
 
 import { _MapContext as MapContext } from "react-map-gl";
-import locations from "../../locations";
+import { gql } from "apollo-boost";
 import { useContext } from "react";
+import { useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+const ROUTE = gql`
+  {
+    route {
+      x
+      y
+    }
+  }
+`;
 
 export default function RouteLine() {
   const mapContext = useContext(MapContext);
-  mapContext.map.addLayer({
-    id: "route",
-    type: "line",
-    source: {
-      type: "geojson",
-      data: {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "LineString",
-          coordinates: locations,
+  const { loading, error, data } = useQuery(ROUTE);
+
+  useEffect(() => {
+    if (!loading && !error)
+      mapContext.map.addLayer({
+        id: "route",
+        type: "line",
+        source: {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: data.route.map(({ x, y }) => [y, x]),
+            },
+          },
         },
-      },
-    },
-    layout: {
-      "line-join": "round",
-      "line-cap": "round",
-    },
-    paint: {
-      "line-color": "#8BC34A",
-      "line-width": 2,
-    },
-  });
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#8BC34A",
+          "line-width": 2,
+        },
+      });
+    // eslint-disable-next-line
+  }, [loading, data]);
+
   return null;
 }
