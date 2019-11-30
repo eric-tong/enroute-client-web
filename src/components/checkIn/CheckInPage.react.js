a90606f59a26bfc2f881fde876efa00ba55b02c1
eric-tong <erictong.designs@gmail.com>
2019-11-30 17:55:17 +0000

    [Check In] Add styles

--- src/components/checkIn/CheckInPage.react.js
+++ src/components/checkIn/CheckInPage.react.js
@@ -6,16 +6,31 @@ type Props = {|
   vehicleId: ?string,
 |};
 
+const options = [
+  {
+    name: "university",
+    message: "I'm a University of Oxford employee or student",
+  },
+  {
+    name: "company",
+    message: "I'm a Begbroke company employee",
+  },
+  {
+    name: "visitor",
+    message: "I'm a visitor",
+  },
+];
+
 export default function CheckInPage({ vehicleId }: Props) {
   return (
     <main className="check-in-container">
       <h1>Check in to bus {vehicleId}</h1>
       <section className="options-container">
-        <div className="option">
-          I'm a University of Oxford employee or student
-        </div>
-        <div className="option">I'm a Begbroke company employee</div>
-        <div className="option">I'm a visitor</div>
+        {options.map(({ name, message }) => (
+          <div key={name} className="option">
+            {message}
+          </div>
+        ))}
       </section>
     </main>
   );
