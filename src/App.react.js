c9b34279409b8d2f6fc590f86b4c6db5e82fdb08
eric-tong <erictong.designs@gmail.com>
2019-11-30 14:33:57 +0000

    [Refactor] Move App to src

--- src/App.react.js
+++ src/App.react.js
@@ -1,9 +1,9 @@
 // @flow
 
-import React from "react";
-import Map from "./map/Map.react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import Map from "./components/map/Map.react";
+import React from "react";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URL,
