1cedd78e8d922c52d6290fa64faf7339f4bc4278
eric-tong <erictong.designs@gmail.com>
2019-12-01 15:19:01 +0000

    [Check In] Add check in confirmation page

--- src/components/checkIn/CheckInPage.react.js
+++ src/components/checkIn/CheckInPage.react.js
@@ -1,37 +1,79 @@
 // @flow
 
-import React from "react";
+import "../../styles/checkmark.scss";
+
+import React, { useState } from "react";
 
 type Props = {|
   vehicleId: ?string,
 |};
+type UserType = $Keys<typeof userTypeNames>;
 
-const options = [
-  {
-    name: "university",
-    message: "I'm a University of Oxford employee or student",
-  },
-  {
-    name: "company",
-    message: "I'm a Begbroke company employee",
-  },
-  {
-    name: "visitor",
-    message: "I'm a visitor",
-  },
-];
+const userTypes: UserType[] = ["university", "company", "visitor"];
+const userTypeNames: { [UserType]: string } = {
+  university: "University of Oxford employee or student",
+  company: "Begbroke company employee",
+  visitor: "visitor",
+};
 
 export default function CheckInPage({ vehicleId }: Props) {
+  const [userType, setUserType] = useState<?UserType>();
+
+  const header = vehicleId ? `Check in to bus ${vehicleId}` : "No bus found";
+  const body = vehicleId ? (
+    userType ? (
+      <ConfirmationSection userType={userType} />
+    ) : (
+      <OptionsList vehicleId={vehicleId} onItemClick={setUserType} />
+    )
+  ) : (
+    undefined
+  );
+
   return (
     <main className="check-in-container">
-      <h1>Check in to bus {vehicleId}</h1>
-      <section className="options-container">
-        {options.map(({ name, message }) => (
-          <div key={name} className="option">
-            {message}
-          </div>
-        ))}
-      </section>
+      <header>
+        <h1>{header}</h1>
+      </header>
+      {body}
     </main>
+  );
+}
+
+function ConfirmationSection({ userType }: { userType: UserType }) {
+  return (
+    <section className="confirmation">
+      <CheckMark isLoading={false} />
+      <h2>Check in successful</h2>
+      <p>Checked in as {userTypeNames[userType]}. Change.</p>
+    </section>
+  );
+}
+
+function CheckMark({ isLoading }: { isLoading: boolean }) {
+  return (
+    <div className={`circle-loader ${!isLoading ? "load-complete" : ""}`}>
+      {!isLoading && <div className="checkmark draw"></div>}
+    </div>
+  );
+}
+
+function OptionsList({
+  vehicleId,
+  onItemClick,
+}: {
+  vehicleId: string,
+  onItemClick: UserType => void,
+}) {
+  return (
+    <section className="options-container">
+      {userTypes.map(type => (
+        <div
+          key={type}
+          onClick={() => onItemClick(type)}
+          className="option"
+        >{`I'm a ${userTypeNames[type]}`}</div>
+      ))}
+    </section>
   );
 }
