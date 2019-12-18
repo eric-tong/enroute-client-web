d79e994c9996398847565cbe509f9606745f1303
eric-tong <erictong.designs@gmail.com>
2019-12-19 07:31:42 +0800

    [History] Display position based on slider

--- src/components/map/HistoryPage.react.js
+++ src/components/map/HistoryPage.react.js
@@ -1,23 +1,57 @@
 // @flow
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import BusStopsOverlay from "./busStops/BusStopsOverlay.react";
 import ReactMapGL from "react-map-gl";
 import TimeTravelSlider from "./history/TimeTravelSlider.react";
-import VehiclesOverlay from "./vehicles/VehiclesOverlay.react";
+import VehicleMarker from "./vehicles/VehicleMarker.react";
+import { gql } from "apollo-boost";
 import initialViewport from "../../styles/viewport";
+import { useQuery } from "@apollo/react-hooks";
+
+const AVLS = gql`
+  {
+    avls {
+      timestamp
+      latitude
+      longitude
+      angle
+    }
+  }
+`;
 
 export default function HistoryPage() {
   const [viewport, setViewport] = useState(initialViewport);
+  const [timeIndex, setTimeIndex] = useState<number>(1);
+  const { loading, error, data } = useQuery(AVLS);
+
+  useEffect(() => {
+    const isLoaded = !loading && !error && data.avls.length > 0;
+    if (isLoaded) setTimeIndex(data.avls.length - 1);
+  }, [data]);
+
+  const isLoaded = !loading && !error && data.avls.length > 0;
+  const index = isLoaded ? Math.min(timeIndex, data?.avls?.length - 1) : 1;
+  const avl = isLoaded ? data.avls[index] : undefined;
 
   return (
     <>
       <ReactMapGL {...viewport} onViewportChange={setViewport}>
-        <VehiclesOverlay />
+        {avl && (
+          <VehicleMarker
+            latitude={avl.latitude}
+            longitude={avl.longitude}
+            bearing={avl.angle}
+          />
+        )}
         <BusStopsOverlay />
       </ReactMapGL>
-      <TimeTravelSlider />
+      <TimeTravelSlider
+        steps={data?.avls?.length}
+        currentIndex={index}
+        onValueChange={setTimeIndex}
+      />
     </>
   );
 }
--- src/components/map/history/TimeTravelSlider.react.js
+++ src/components/map/history/TimeTravelSlider.react.js
@@ -2,20 +2,29 @@
 
 import "../../../styles/time-travel-slider.scss";
 
-import React, { useState } from "react";
+import React from "react";
 
-export default function TimeTravelSlider() {
-  const [time, setTime] = useState(50);
+type Props = {
+  steps: number,
+  currentIndex: number,
+  onValueChange: number => void,
+};
 
+export default function TimeTravelSlider({
+  steps = 1,
+  currentIndex,
+  onValueChange,
+}: Props) {
   return (
     <div className="time-travel-slider-container">
       <div className="card">
         <input
+          disabled={steps < 2}
           type="range"
-          min="1"
-          max="100"
-          value={time}
-          onChange={event => setTime(event.target.value)}
+          min="0"
+          max={steps}
+          value={currentIndex}
+          onChange={event => onValueChange(event.target.value)}
           className="slider"
         />
       </div>
