0ae586745f9a0af87731aa4434dc7bcfc075f5d3
eric-tong <erictong.designs@gmail.com>
2020-02-12 15:37:33 +0000

    [Mobile] Refactor map into separate component

---
+++ src/pages/map/Map.react.js
@@ -0,0 +1,34 @@
+// @flow
+
+import React, { useEffect, useState } from "react";
+
+import BusStopsOverlay from "./busStops/BusStopsOverlay.react";
+import ReactMapGL from "react-map-gl";
+import RouteLine from "./RouteLine.react";
+import VehiclesOverlay from "./vehicles/VehiclesOverlay.react";
+import useViewport from "../../utils/useViewport";
+
+export default function Map() {
+  const [viewport, setViewport] = useViewport();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!viewport) {
+      setIsLoaded(false);
+    }
+  }, [viewport]);
+
+  if (viewport)
+    return (
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        onLoad={() => setIsLoaded(true)}
+      >
+        {isLoaded && <RouteLine />}
+        <VehiclesOverlay />
+        <BusStopsOverlay />
+      </ReactMapGL>
+    );
+  else return null;
+}
--- src/pages/map/MapPage.react.js
+++ src/pages/map/MapPage.react.js
@@ -2,42 +2,19 @@
 
 import "../../styles/panel.scss";
 
-import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import BusStopDetailView from "./panel/BusStopDetailView.react";
-import BusStopsOverlay from "./busStops/BusStopsOverlay.react";
 import DepartureBoard from "./panel/DepartureBoard.react";
+import Map from "./Map.react";
 import MenuButton from "../misc/MenuButton.react";
 import { PANEL_WIDTH } from "../../utils/useViewport";
-import ReactMapGL from "react-map-gl";
-import RouteLine from "./RouteLine.react";
-import VehiclesOverlay from "./vehicles/VehiclesOverlay.react";
-import useViewport from "../../utils/useViewport";
+import React from "react";
 
-function Map() {
-  const [viewport, setViewport] = useViewport();
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    if (!viewport) {
-      setIsLoaded(false);
-    }
-  }, [viewport]);
-
+function MapPage() {
   return (
     <>
-      {viewport && (
-        <ReactMapGL
-          {...viewport}
-          onViewportChange={setViewport}
-          onLoad={() => setIsLoaded(true)}
-        >
-          {isLoaded && <RouteLine />}
-          <VehiclesOverlay />
-          <BusStopsOverlay />
-        </ReactMapGL>
-      )}
+      <Map />
       <section id="panel" style={{ width: PANEL_WIDTH }}>
         <MenuButton />
         <Switch>
@@ -56,4 +33,4 @@ function Map() {
   );
 }
 
-export default Map;
+export default MapPage;
